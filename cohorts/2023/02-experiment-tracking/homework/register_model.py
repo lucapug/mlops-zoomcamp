1b4bb56d8cd29ad65377b402403c36b44943cb86
lucapug <lucapug@gmail.com>
2023-05-29 11:15:57 +0200

    executed and modified register_model for hw2

--- cohorts/2023/02-experiment-tracking/homework/register_model.py
+++ cohorts/2023/02-experiment-tracking/homework/register_model.py
@@ -63,17 +63,24 @@ def run_register_model(data_path: str, top_n: int):
         experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=top_n,
-        order_by=["metrics.rmse ASC"]
+        order_by=["metrics.val_rmse ASC"]
     )
     for run in runs:
         train_and_log_model(data_path=data_path, params=run.data.params)
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        order_by=["metrics.test_rmse ASC"]        
+        )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    run_id = best_run.info.run_id
+    model_uri = f"runs:/{run_id}/model"
+    mlflow.register_model(model_uri=model_uri, name="best_model_rf")
+
 
 
 if __name__ == '__main__':
