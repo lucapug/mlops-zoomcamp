313c770fc1ff6c86cdbb528c821791821b07fd49
lucapug <lucapug@gmail.com>
2023-06-26 11:44:36 +0200

    clean score.py obtained nbconvert

--- cohorts/2023/04-deployment/my_hw/score.py
+++ cohorts/2023/04-deployment/my_hw/score.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-get_ipython().system('pip freeze | grep scikit-learn')
-
-
-# In[27]:
-
-
-import os
 import pickle
 import pandas as pd
-
-
-# In[3]:
-
 
 year = 2022
 month = 2
 
-
-# In[20]:
-
-
 get_ipython().system("mkdir './output'")
-
-
-# In[11]:
-
 
 input_file=f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
 output_file=f'./output/preds_{year:04d}-{month:02d}.parquet'
 
-
-# In[4]:
-
-
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
-
-
-# In[12]:
-
 
 categorical = ['PULocationID', 'DOLocationID']
 
@@ -60,16 +30,8 @@ def read_data(filename):
     return df
 
 
-# In[13]:
-
-
 #df = read_data(f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet')
 df = read_data(input_file)
-#df = read_data(f's3://lucap-nyc-taxi-data/data/yellow/yellow_tripdata_{year:04d}-{month:02d}.parquet')
-#df = read_data ('s3://lucap-nyc-taxi-data/data/yellow/yellow_tripdata_2022-02.parquet')
-
-
-# In[14]:
 
 
 dicts = df[categorical].to_dict(orient='records')
@@ -77,39 +39,17 @@ X_val = dv.transform(dicts)
 y_pred = model.predict(X_val)
 
 
-# In[15]:
-
-
 y_pred.std()
-
-
-# In[16]:
 
 
 df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
 
-# In[23]:
-
-
 df_result = pd.DataFrame()
-
-
-# In[24]:
 
 
 df_result['ride_id'] = df['ride_id']
 df_result['prediction'] = y_pred
-
-
-# In[25]:
-
-
-df_result.head()
-
-
-# In[26]:
-
 
 df_result.to_parquet(
     output_file,
@@ -117,10 +57,3 @@ df_result.to_parquet(
     compression=None,
     index=False
 )
-
-
-# In[28]:
-
-
-os.stat(output_file)
-
