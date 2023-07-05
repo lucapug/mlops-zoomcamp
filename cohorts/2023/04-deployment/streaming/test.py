4bd9e121ba0b3e39be824c68babe34bcb8c314c7
lucapug <lucapug@gmail.com>
2023-07-05 15:49:01 +0000

    update records with my data eu-south-1

--- cohorts/2023/04-deployment/streaming/test.py
+++ cohorts/2023/04-deployment/streaming/test.py
@@ -7,17 +7,17 @@ event = {
             "kinesis": {
                 "kinesisSchemaVersion": "1.0",
                 "partitionKey": "1",
-                "sequenceNumber": "49630081666084879290581185630324770398608704880802529282",
-                "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDI1NgogICAgfQ==",
-                "approximateArrivalTimestamp": 1654161514.132
+                "sequenceNumber": "49642333528152160045161343175597482755845342796282068994",
+                "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDE1NgogICAgfQ==",
+                "approximateArrivalTimestamp": 1688499216.333
             },
             "eventSource": "aws:kinesis",
             "eventVersion": "1.0",
-            "eventID": "shardId-000000000000:49630081666084879290581185630324770398608704880802529282",
+            "eventID": "shardId-000000000000:49642333528152160045161343175597482755845342796282068994",
             "eventName": "aws:kinesis:record",
-            "invokeIdentityArn": "arn:aws:iam::387546586013:role/lambda-kinesis-role",
-            "awsRegion": "eu-west-1",
-            "eventSourceARN": "arn:aws:kinesis:eu-west-1:387546586013:stream/ride_events"
+            "invokeIdentityArn": "arn:aws:iam::188124115410:role/lambda-kinesis-role",
+            "awsRegion": "eu-south-1",
+            "eventSourceARN": "arn:aws:kinesis:eu-south-1:188124115410:stream/ride_events"
         }
     ]
 }
