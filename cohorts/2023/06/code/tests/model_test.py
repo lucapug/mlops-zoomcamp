612753b3d8aec56cc50a5d7a22f8833b2235f792
lucapug <lucapug@gmail.com>
2023-07-08 09:50:58 +0000

    test1 prepare_features

--- cohorts/2023/06/code/tests/model_test.py
+++ cohorts/2023/06/code/tests/model_test.py
@@ -1,7 +1,20 @@
-#import lambda_function
+import model
 
-#lambda_function.prepare_features()
+def test_prepare_features():
 
-def test_temp():
-    assert 1==1
+    model_service = model.ModelService(None)
     
+    ride = {
+            "PULocationID": 130,
+            "DOLocationID": 205,
+            "trip_distance": 3.66
+        }
+    
+    actual_features = model_service.prepare_features(ride)
+    
+    expected_features = {
+        "PU_DO": "130_205",
+        "trip_distance": 3.66
+    }
+    
+    assert actual_features == expected_features
