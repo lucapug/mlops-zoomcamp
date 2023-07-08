f22bae50a850c0f66feb4fba0f9598dfce55a5c7
lucapug <lucapug@gmail.com>
2023-07-08 13:56:09 +0000

    test4 test lambda_handler()

--- cohorts/2023/06/code/tests/model_test.py
+++ cohorts/2023/06/code/tests/model_test.py
@@ -58,3 +58,36 @@ def test_predict():
 
     
     assert actual_preds == expected_preds
+
+def test_lambda_handler():
+
+    model_mock = ModelMock(10.0)
+    model_version = 'Test123'
+    model_service = model.ModelService(model_mock, model_version)
+    
+    event = {
+        "Records": [
+            {
+                "kinesis": {
+                    "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDE1NgogICAgfQ==",
+                },
+            }
+        ]
+    }   
+    
+    actual_preds = model_service.lambda_handler(event)
+    expected_preds = {
+        'predictions': [
+            {
+                'model': 'ride_duration_prediction_model',
+                'version': model_version,
+                'prediction': {
+                    'ride_duration': 10.0,
+                    'ride_id': 156,
+                },
+            }
+        ]
+    }
+
+    
+    assert actual_preds == expected_preds    
