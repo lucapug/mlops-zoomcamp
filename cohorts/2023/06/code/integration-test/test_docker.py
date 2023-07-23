d69bb914ca596eb326ce36a112f6103cbb78caad
lucapug <lucapug@gmail.com>
2023-07-23 16:04:00 +0000

    add commas in multiline fro black

--- cohorts/2023/06/code/integration-test/test_docker.py
+++ cohorts/2023/06/code/integration-test/test_docker.py
@@ -1,29 +1,29 @@
-#pylint: disable=duplicate-code
-#pylint: disable=missing-timeout
+# pylint: disable=duplicate-code
+# pylint: disable=missing-timeout
 
 import json
 
 import requests
 from deepdiff import DeepDiff
 
-with open('event.json', 'rt', encoding='utf-8') as f_in:
+with open("event.json", "rt", encoding="utf-8") as f_in:
     event = json.load(f_in)
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+url = "http://localhost:8080/2015-03-31/functions/function/invocations"
 actual_response = requests.post(url, json=event).json()
 
-print('actual response:')
+print("actual response:")
 
 print(json.dumps(actual_response, indent=2))
 
 expected_response = {
     "predictions": [
         {
-            'model': 'ride_duration_prediction_model',
-            'version': 'Test123',
-            'prediction': {
-                'ride_duration': 18.2,
-                'ride_id': 156   
+            "model": "ride_duration_prediction_model",
+            "version": "Test123",
+            "prediction": {
+                "ride_duration": 18.2,
+                "ride_id": 156,
             },
         }
     ]
@@ -31,7 +31,7 @@ expected_response = {
 
 
 diff = DeepDiff(actual_response, expected_response, significant_digits=1)
-print(f'diff={diff}')
+print(f"diff={diff}")
 
-assert 'type_changes' not in diff
-assert 'values_changed' not in diff
+assert "type_changes" not in diff
+assert "values_changed" not in diff
