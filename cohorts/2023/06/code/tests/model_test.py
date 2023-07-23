59a20ed6eb1de5c25d6374cea08589b221086236
lucapug <lucapug@gmail.com>
2023-07-23 18:02:25 +0000

    add comma for black

--- cohorts/2023/06/code/tests/model_test.py
+++ cohorts/2023/06/code/tests/model_test.py
@@ -2,78 +2,69 @@ from pathlib import Path
 
 import model
 
+
 def read_text(file):
     test_directory = Path(__file__).parent
 
-    with open(test_directory / file, 'rt', encoding='utf-8') as f_in:
+    with open(test_directory / file, "rt", encoding="utf-8") as f_in:
         return f_in.read().strip()
 
+
 def test_base64_decode():
-    base64_input = read_text('data.b64')
+    base64_input = read_text("data.b64")
     actual_result = model.base64_decode(base64_input)
     expected_result = {
         "ride": {
             "PULocationID": 130,
             "DOLocationID": 205,
-            "trip_distance": 3.66
-        }, 
-        "ride_id": 156
+            "trip_distance": 3.66,
+        },
+        "ride_id": 156,
     }
-    
+
     assert actual_result == expected_result
 
-def test_prepare_features():
 
+def test_prepare_features():
     model_service = model.ModelService(None)
-    
-    ride = {
-            "PULocationID": 130,
-            "DOLocationID": 205,
-            "trip_distance": 3.66
-        }
-    
+
+    ride = {"PULocationID": 130, "DOLocationID": 205, "trip_distance": 3.66}
+
     actual_features = model_service.prepare_features(ride)
-    
-    expected_features = {
-        "PU_DO": "130_205",
-        "trip_distance": 3.66
-    }
-    
+
+    expected_features = {"PU_DO": "130_205", "trip_distance": 3.66}
+
     assert actual_features == expected_features
-    
-class ModelMock():
+
+
+class ModelMock:
     def __init__(self, value) -> None:
         self.value = value
-    
+
     def predict(self, X):
         n = len(X)
-        return [self.value]*n
+        return [self.value] * n
 
 
 def test_predict():
-
     model_mock = ModelMock(10.0)
     model_service = model.ModelService(model_mock)
-    
-    features = {
-        "PU_DO": "130_205",
-        "trip_distance": 3.66
-    }
-    
+
+    features = {"PU_DO": "130_205", "trip_distance": 3.66}
+
     actual_preds = model_service.predict(features)
     expected_preds = 10.0
 
-    
     assert actual_preds == expected_preds
 
-def test_lambda_handler():
 
+def test_lambda_handler():
     model_mock = ModelMock(10.0)
-    model_version = 'Test123'
+    model_version = "Test123"
     model_service = model.ModelService(model_mock, model_version)
-    
-    base64_input = read_text('data.b64')    
-    
+
+    base64_input = read_text("data.b64")
+
     event = {
         "Records": [
             {
@@ -82,21 +73,20 @@ def test_lambda_handler():
                 },
             }
         ]
-    }   
-    
+    }
+
     actual_preds = model_service.lambda_handler(event)
     expected_preds = {
-        'predictions': [
+        "predictions": [
             {
-                'model': 'ride_duration_prediction_model',
-                'version': model_version,
-                'prediction': {
-                    'ride_duration': 10.0,
-                    'ride_id': 156,
+                "model": "ride_duration_prediction_model",
+                "version": model_version,
+                "prediction": {
+                    "ride_duration": 10.0,
+                    "ride_id": 156,
                 },
             }
         ]
     }
 
-    
-    assert actual_preds == expected_preds    
+    assert actual_preds == expected_preds
